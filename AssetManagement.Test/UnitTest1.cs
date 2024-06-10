ec0e7eed63d659435f01448ff6c540b173c864f5
Fatemeh Khodadadi <fatemeh.khodadadi@outlook.com>
2024-06-10 16:49:15 +0100

    feat(AM):Adds Asset Management system

--- AssetManagement.Test/UnitTest1.cs
+++ AssetManagement.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Xunit;
+
 namespace AssetManagement.Test;
 
 public class UnitTest1
--- AssetManagement/obj/Debug/net8.0/AssetManagement.AssemblyInfo.cs
+++ AssetManagement/obj/Debug/net8.0/AssetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4f14babc509d7e71c525deec15982c9be4d841")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
